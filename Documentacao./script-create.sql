bd936f6bceb4d2f90b65fec83cfc6f85f77561b8
Pedro Henrique Medeiros Hermes <79016964+pedrohermes22@users.noreply.github.com>
2022-10-13 13:06:50 -0300

    Adicionando AUTO_INCREMENT.

--- Documentacao./script-create.sql
+++ Documentacao./script-create.sql
@@ -1,29 +1,29 @@
 CREATE SCHEMA ambiente;
 
 CREATE TABLE ambiente.regiao (
-    id INT,
+    id INT AUTO_INCREMENT,
     nome VARCHAR(25),
     CONSTRAINT pk_regiao PRIMARY KEY(id),
-    CONSTRAINT uk_nome UNIQUE(nome)
+    CONSTRAINT uk_regiao_nome UNIQUE(nome)
 );
 
 CREATE TABLE ambiente.estado (
-    id INT,
+    id INT AUTO_INCREMENT,
     sigla VARCHAR(2),
     nome VARCHAR(50),
     id_regiao INT,
     CONSTRAINT pk_estado PRIMARY KEY(id),
-    CONSTRAINT fk_estado_regiao FOREIGN KEY(id_regiao)
-        REFERENCES ambiente.regiao(id)
+    CONSTRAINT uk_estado_sigla UNIQUE (sigla),
+    CONSTRAINT uk_estado_nome UNIQUE (nome),
+    CONSTRAINT fk_estado_regiao FOREIGN KEY(id_regiao) REFERENCES ambiente.regiao(id)
 );
 
 CREATE TABLE ambiente.municipio (
-    id INT,
+    id INT AUTO_INCREMENT,
     nome VARCHAR(100),
     id_estado INT,
     CONSTRAINT pk_municipio PRIMARY KEY(id),
-    CONSTRAINT fk_municipio_estado FOREIGN KEY(id_estado)
-        REFERENCES ambiente.estado(id)
+    CONSTRAINT fk_municipio_estado FOREIGN KEY(id_estado) REFERENCES ambiente.estado(id)
 );
 
 CREATE TABLE ambiente.estacao_metereologica (
@@ -31,8 +31,7 @@ CREATE TABLE ambiente.estacao_metereologica (
     nome VARCHAR(100),
     id_municipio INT,
     CONSTRAINT pk_estacoes_metereologicas PRIMARY KEY(codigo),
-    CONSTRAINT fk_estacoes_metereologicas_municipio FOREIGN KEY(id_municipio)
-        REFERENCES ambiente.municipio(id)
+    CONSTRAINT fk_estacoes_metereologicas_municipio FOREIGN KEY(id_municipio) REFERENCES ambiente.municipio(id)
 );
 
 CREATE TABLE ambiente.medicao_clima (
@@ -41,37 +40,33 @@ CREATE TABLE ambiente.medicao_clima (
     codigo_estacao_metereologica VARCHAR(100),
     temperatura_minina FLOAT,
     temperatura_maxima FLOAT,
-    CONSTRAINT fk_medicoes_clima_estacao_metereologica FOREIGN KEY(codigo_estacao_metereologica)
-        REFERENCES ambiente.estacao_metereologica(codigo),
+    CONSTRAINT fk_medicoes_clima_estacao_metereologica FOREIGN KEY(codigo_estacao_metereologica) REFERENCES ambiente.estacao_metereologica(codigo),
     CONSTRAINT pk_medicoes_clima PRIMARY KEY(codigo_estacao_metereologica, dt, hora)
 );
 
 CREATE TABLE ambiente.area_geografica (
-    id INT,
-    area_total INT,
+    id INT AUTO_INCREMENT,
+    area_total FLOAT,
     id_municipio INT,
     CONSTRAINT pk_area_geografica PRIMARY KEY(id),
-    CONSTRAINT fk_area_geografica_municipio FOREIGN KEY(id_municipio)
-        REFERENCES ambiente.municipio(id)
+    CONSTRAINT fk_area_geografica_municipio FOREIGN KEY(id_municipio) REFERENCES ambiente.municipio(id)
 );
 
 CREATE TABLE ambiente.floresta (
-    id INT,
+    id INT AUTO_INCREMENT,
     area_floresta FLOAT,
     area_nao_floresta FLOAT,
     ano INT,
     id_area_geografica INT,
     CONSTRAINT pk_floresta PRIMARY KEY(id),
-    CONSTRAINT fk_floresta_area_geografica FOREIGN KEY(id_area_geografica)
-        REFERENCES ambiente.area_geografica(id)
+    CONSTRAINT fk_floresta_area_geografica FOREIGN KEY(id_area_geografica) REFERENCES ambiente.area_geografica(id)
 );
 
 CREATE TABLE ambiente.desmatamento (
-    id INT,
+    id INT AUTO_INCREMENT,
     taxa_incremento FLOAT,
     area_desmatada FLOAT,
     id_floresta INT,
-    CONSTRAINT fk_desmatamento_floresta FOREIGN KEY(id_floresta)
-        REFERENCES ambiente.floresta(id),
+    CONSTRAINT fk_desmatamento_floresta FOREIGN KEY(id_floresta) REFERENCES ambiente.floresta(id),
     CONSTRAINT pk_desmatamento PRIMARY KEY(id_floresta, id)
 );
